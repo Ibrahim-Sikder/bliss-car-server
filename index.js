c8941405404778d9d663a0bdc31cfebec0cea0be
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-11-26 15:56:53 +0600

    car-category-api-added

--- index.js
+++ index.js
@@ -17,6 +17,67 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
+async function run(){
+    try{
+        const carCollection = client.db('CarProducts').collection('catCategories');
+        const luxuryCarCollection = client.db('CarProducts').collection('LuxuryCar');
+        const electricCarCollection = client.db('CarProducts').collection('electricCars');
+      
+
+
+   // for home page service find  query
+         app.get('/categories', async(req, res)=>{
+            const query = {} 
+            const cursor = carCollection.find(query);
+            const categories  = await cursor.limit(3).toArray();
+            res.send(categories);
+        })
+        // for microbus 
+        app.get('/microbusCar', async(req, res)=>{
+            const query = {} 
+            const cursor = luxuryCarCollection.find(query);
+            const categories  = await cursor.limit(3).toArray();
+            res.send(categories);
+        })
+
+        app.get('/electricCars', async(req, res)=>{
+            const query = {} 
+            const cursor = electricCarCollection.find(query);
+            const categories  = await cursor.limit(3).toArray();
+            res.send(categories);
+        })
+    
+    
+        
+    
+      
+      
+        // see all service 
+        // app.get('/allService', async(req, res)=>{
+        //     const query = {} 
+        //     const cursor = serviceCollection.find(query);
+        //     const services  = await cursor.toArray();
+        //     res.send(services);
+        // })
+    
+       // service details api 
+    
+        app.get('/services/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query) ;
+            res.send(service)
+        })
+       
+       
+    
+    }
+    finally{
+    
+    }
+    }
+    run().catch(err=>console.log(err))
+
 
 
 app.get('/', (req, res)=>{
