0a5be86bc64a5f2143b3744e2b1e36db85c85569
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-11-26 15:45:30 +0600

    database-connection-complete

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.use(cors())
 app.use(express.json())
 
 // database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.u82gun1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 
 app.get('/', (req, res)=>{
